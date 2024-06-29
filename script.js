7a2d94a4eb6aef34e7fea5851ce00cd2a335931d
Max Lazaro <maxlazaromax@gmail.com>
2024-06-29 18:45:29 -0300

    salvando Lógica de Programação

---
+++ script.js
@@ -0,0 +1,127 @@
+// # 1️⃣ Desafio Classificador de nível de Herói
+
+// **O Que deve ser utilizado**
+
+// - Variáveis
+// - Operadores
+// - Laços de repetição
+// - Estruturas de decisões
+
+// ## Objetivo
+
+// Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
+
+// Se XP for menor do que 1.000 = Ferro
+// Se XP for entre 1.001 e 2.000 = Bronze
+// Se XP for entre 2.001 e 5.000 = Prata
+// Se XP for entre 5.001 e 7.000 = Ouro
+// Se XP for entre 7.001 e 8.000 = Platina
+// Se XP for entre 8.001 e 9.000 = Ascendente
+// Se XP for entre 9.001 e 10.000= Imortal
+// Se XP for maior ou igual a 10.001 = Radiante
+
+// ## Saída
+
+// Ao final deve se exibir uma mensagem:
+// "O Herói de nome **{nome}** está no nível de **{nivel}**"
+
+
+
+
+console.log("cgfnb v hvbdbdg")
+
+
+
+
+
+
+
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+// let nomeUsuario = "Fernando"
+// let nivelExperiencia = 7000
+
+// if (nivelExperiencia <= 1.000){
+//     console.log(" Se XP for menor do que 1.000 = Ferro")
+// }else if (nivelExperiencia <= 2.000){
+//     console.log("Se XP for entre 1.001 e 2.000 = Bronze")
+// }else if (nivelExperiencia <= 5.000){
+//     console.log("Se XP for entre 2.001 e 5.000 = Prata")
+// }else if (nivelExperiencia <= 7.000){
+//     console.log("Se XP for entre 5.001 e 7.000 = Ouro")
+// }
+
+// console.log("O Herói de nome " + nomeUsuario+ "tem em média " + nivelExperiencia)
+
+
+
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+//   let nome = "pedro"
+//   let mediaExperiencia = 1000
+
+//   switch (mediaExperiencia){
+//     case 1000:
+//         console.log (" Se XP for menor do que 1.000 = Ferro")
+//         break
+//     case 2000:
+//         console.log  ("Se XP for entre 1.001 e 2.000 = Bronze")
+//         break
+//     case 5000:
+//         console.log (" Se XP for entre 2.001 e 5.000 = Prata")
+//         break    
+
+//     case 7000:
+//         console.log (" Se XP for entre 5.001 e 7.000 = Ouro")
+//         break    
+
+//     case "Se XP for entre 7.001 e 8.000 = Platina":
+//         console.log ("evoluindo 1")
+//         break    
+
+//     case "Se XP for entre 8.001 e 9.000 = Ascendente":
+//         console.log ("evoluindo 1")
+//         break    
+
+//     case "Se XP for entre 9.001 e 10.000= Imortal":
+//         console.log ("evoluindo 1")
+//         break    
+
+//     case "Se XP for maior ou igual a 10.001 = Radiante":
+//         console.log ("evoluindo 1")
+//         break 
+//   }
+
+//   console.log ("O Herói de nome: " +nome+ " tem em média " +mediaExperiencia+ " pontos")
+
+
+
+// --------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
